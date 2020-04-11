49e5fb0c91e2c948776a50ce7bf001d7a038ac2c
Bartosz Kahl <bartosz.kahl@gmail.com>
2020-04-12 00:20:33 +0200

    fix: fix terraform download script

--- scripts/install-terraform.sh
+++ scripts/install-terraform.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TERRAFORM_VERSION = $(cat ./.terraform-version)
+TERRAFORM_VERSION=$(cat .terraform-version)
 
 wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
